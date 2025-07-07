ab34e30d8f109ba58e46ecad7ebd5f269ca90c47
omar-radwan7 <omarobrashy2004@gmail.com>
2025-07-07 22:00:33 +0200

    oragnized the code and updated the stock details page

--- stock_market_website/server.js
+++ stock_market_website/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const FMP_API_KEY = 'j1u46LGOa7YdHb8C0Lc6sE3yCLBffM91';
+const FMP_API_KEY = 'ty7RTtW3cMOnH3eizxFHEFZ4qjZ4xkYw';
 const FMP_BASE_URL = 'https://financialmodelingprep.com/api/v3';
 
 app.use(cors());
